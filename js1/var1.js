01c22069e6d81bc7b20918c60788b710081d33b9
Abhishek Gharami <abhishekgharami1998@gmail.com>
2022-09-30 17:01:30 +0530

    adding file

---
+++ js1/var1.js
@@ -0,0 +1,32 @@
+/*
+Primitve datatypes of the javascripts are:
+1. Number
+2. String
+3.Boolean
+4. Null
+5.Undefined.
+6. Symbol
+7. BigInt
+*/
+// Introduction of Number
+let age = 12;
+console.log(age);
+
+let hens = 12;
+let chicken = 23;
+var sum = hens+chicken;
+console.log(sum);
+// const variable
+// In const variable you can't change the value of the variable but in let you can once you can committed.
+const var1 = 13.5;
+console.log(var1);
+console.log(typeof(var1));
+//This will throw error
+/*var1 = var1+1;
+try{
+ console.log(var1);
+}
+catch(error){
+  console.log(error);
+}*/
+
