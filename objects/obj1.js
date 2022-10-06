e48ea39ac1f87a0d26187606cf589a43e3d4d309
Abhishek Gharami <abhishekgharami1998@gmail.com>
2022-10-06 10:59:42 +0530

    working with objects

---
+++ objects/obj1.js
@@ -0,0 +1,15 @@
+let obj = {
+    first_name : 'Abhishek',
+    last_name : 'Gharami',
+    lang : ['ben','eng','hind'],
+    code : ['python','java','javascript','c','c++'],
+    func1(){
+      console.log("first function.");
+    }
+
+};
+
+//acessing the objects
+console.log(obj.first_name);
+console.log(obj.last_name);
+console.log(obj.func1());
