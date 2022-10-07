79a4ae941fc732e29bbddd677227d040e7e43d91
Abhishek Gharami <abhishekgharami1998@gmail.com>
2022-10-07 13:42:39 +0530

    sort reverse-sort

---
+++ arrays/sort1.js
@@ -0,0 +1,8 @@
+//sort method
+let arr = [400.50,5000,99.99,35.99,12.00,9500];
+let num1 = arr.sort((a,b)=>a-b);
+console.log(num1);
+
+//reverse sort a number
+num1 = arr.sort((a,b)=>b-a);
+console.log(num1);
